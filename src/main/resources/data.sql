24ba080f7e8546270b8316ec5e248e607c2f5cd4
drovocek <mycodepg@gmail.com>
2020-12-29 13:40:14 +0300

    init git

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS (PHONE_NUMBER, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES ('+1 (111) 111-11-11', 'user1@gmail.com', 'User', 'First'),
+       ('+2 (222) 222-22-22', 'user2@gmail.com', 'User', 'Second');
---
+++ src/test/java/edu/volkov/userapp/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.volkov.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
