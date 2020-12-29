8695ba8c1134cc10841824dcae0452b238ff4f0e
drovocek <mycodepg@gmail.com>
2020-12-29 15:58:20 +0300

    populate ref id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO USERS (ID, PHONE_NUMBER, EMAIL, FIRST_NAME, LAST_NAME)
-VALUES (1, '+1 (111) 111-11-11', 'mail1@gmail.com', 'First_name1', 'Last_Name1'),
-       (2, '+2 (222) 222-22-22', 'mail2@gmail.com', 'First_name2', 'Last_Name2'),
-       (3, '+3 (333) 333-33-33', 'mail3@gmail.com', 'First_name3', 'Last_Name3'),
-       (4, '+4 (444) 444-44-44', 'mail4@gmail.com', 'First_name4', 'Last_Name4'),
-       (5, '+5 (555) 555-55-55', 'mail5@gmail.com', 'First_name5', 'Last_Name5'),
-       (6, '+6 (666) 666-66-66', 'mail6@gmail.com', 'First_name6', 'Last_Name6'),
-       (7, '+7 (777) 777-77-77', 'mail7@gmail.com', 'First_name7', 'Last_Name7'),
-       (8, '+8 (888) 888-88-88', 'mail8@gmail.com', 'First_name8', 'Last_Name8'),
-       (9, '+9 (999) 999-99-99', 'mail9@gmail.com', 'First_name9', 'Last_Name9'),
-       (10, '+0 (000) 000-00-00', 'mail10@gmail.com', 'First_name10', 'Last_Name10');
+INSERT INTO USERS (PHONE_NUMBER, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES ('+1 (111) 111-11-11', 'mail1@gmail.com', 'First_name1', 'Last_Name1'),
+       ('+2 (222) 222-22-22', 'mail2@gmail.com', 'First_name2', 'Last_Name2'),
+       ('+3 (333) 333-33-33', 'mail3@gmail.com', 'First_name3', 'Last_Name3'),
+       ('+4 (444) 444-44-44', 'mail4@gmail.com', 'First_name4', 'Last_Name4'),
+       ('+5 (555) 555-55-55', 'mail5@gmail.com', 'First_name5', 'Last_Name5'),
+       ('+6 (666) 666-66-66', 'mail6@gmail.com', 'First_name6', 'Last_Name6'),
+       ('+7 (777) 777-77-77', 'mail7@gmail.com', 'First_name7', 'Last_Name7'),
+       ('+8 (888) 888-88-88', 'mail8@gmail.com', 'First_name8', 'Last_Name8'),
+       ('+9 (999) 999-99-99', 'mail9@gmail.com', 'First_name9', 'Last_Name9'),
+       ('+0 (000) 000-00-00', 'mail10@gmail.com', 'First_name10', 'Last_Name10');
