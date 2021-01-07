de6c1e19beb43aeb7846c2d4a79cf95633288916
drovocek <mycodepg@gmail.com>
2021-01-07 15:10:06 +0300

    before merg

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -49,7 +49,116 @@ const restApi = {
     }
 }
 
-const socketApi = {}
+var stompClient = null;
+const greetPanel = $("#greetings");
+
+const socketApi = {
+    setConnected(connected) {
+        console.log("setConnected(connected): " + connected);
+        $("#connect").prop("disabled", connected);
+        $("#disconnect").prop("disabled", !connected);
+        if (connected) {
+            $("#conversation").show();
+        } else {
+            $("#conversation").hide();
+        }
+        $("#greetings").html("");
+    },
+    connect() {
+        console.log("connect()");
+        var socket = new SockJS('/gs-guide-websocket');
+        console.log("socket: " + socket);
+        stompClient = Stomp.over(socket);
+        stompClient.connect({}, function (frame) {
+            socketApi.setConnected(true);
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/topic/greetings', function (greeting) {
+                console.log("RESPONSE");
+                console.log(greeting.body);
+                // showGreeting(JSON.parse(greeting.body).email);
+                const greetBody = JSON.parse(greeting.body);
+                switch (greetBody.packageType) {
+                    case 'GET':
+                        socketApi.showGet(greetBody);
+                        break;
+                    case 'GET_ALL':
+                        socketApi.showGetAll(greetBody);
+                        break;
+                    case 'UPDATE':
+                        socketApi.showUpdate(greetBody);
+                        break;
+                    case 'CREATE':
+                        socketApi.showCreate(greetBody);
+                        break;
+                    case 'DELETE':
+                        socketApi.showDelete(greetBody);
+                        break;
+                    default:
+                        alert('NO RESPONSE TYPE');
+                }
+            });
+        });
+    },
+    disconnect() {
+        console.log("disconnect()");
+
+        if (stompClient !== null) {
+            stompClient.disconnect();
+        }
+        socketApi.setConnected(false);
+        console.log("Disconnected");
+    },
+    showUpdate(greetBody) {
+        console.log("showUpdate()");
+        greetPanel.append("<tr><td>" + greetBody.id + "</td></tr>");
+        greetPanel.append("<tr><td>" + greetBody.firstName + "</td></tr>");
+        greetPanel.append("<tr><td>" + greetBody.lastName + "</td></tr>");
+        greetPanel.append("<tr><td>" + greetBody.phoneNumber + "</td></tr>");
+        greetPanel.append("<tr><td>" + greetBody.email + "</td></tr>");
+    },
+    sendName() {
+        console.log("sendName()");
+
+        console.log("stompClient: " + stompClient);
+        // stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+        stompClient.send("/app/users/update", {},
+            // JSON.stringify({'id': "1", 'firstName':"",'lastName':"",'phoneNumber':"",'email':""}));
+            JSON.stringify({
+                'id': "1",
+                'firstName': "newFirstName",
+                'lastName': "newLastName",
+                'phoneNumber': "9 (999) 999-99-99",
+                'email': "andrey@gmail.com"
+            }));
+    },
+    showGreeting(message) {
+        console.log("showGreeting()");
+
+        $("#greetings").append("<tr><td>" + message + "</td></tr>");
+    }
+}
+$(function () {
+    console.log("START");
+    $("form").on('submit', function (e) {
+        console.log("START preventDefault()");
+        e.preventDefault();
+    });
+
+    $("#connect").click(function () {
+        console.log("START connect()");
+        socketApi.connect();
+    });
+
+    $("#disconnect").click(function () {
+        console.log("START disconnect()");
+        socketApi.disconnect();
+    });
+
+    $("#send").click(function () {
+        console.log("START sendName()");
+        socketApi.sendName();
+    });
+});
 
 const viewApi = {
     initTableView() {
