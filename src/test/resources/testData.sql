ad8c29406c7563e23e0d60342530bc3597a9d6fe
drovocek <mycodepg@gmail.com>
2021-01-10 15:45:05 +0300

    migrate to postgresql

--- src/test/resources/testData.sql
+++ src/test/resources/testData.sql
@@ -1,7 +1,10 @@
-DELETE FROM USERS;
-ALTER TABLE USERS ALTER COLUMN ID RESTART WITH 1;
+DELETE
+FROM USERS;
 
-INSERT INTO USERS (PHONE_NUMBER, EMAIL, FIRST_NAME, LAST_NAME)
+ALTER TABLE USERS
+    ALTER COLUMN ID RESTART WITH 1;
 
-VALUES ('+1 (111) 111-11-11', 'vasily@gmail.com', 'Vasily', 'Ivanov'),
-       ('+2 (222) 222-22-22', 'ivan@gmail.com', 'Ivan', 'Vasiliev');
+INSERT INTO USERS (first_name, last_name, phone_number, email)
+
+VALUES ('Vasily', 'Ivanov', '+1 (111) 111-11-11', 'vasily@gmail.com'),
+       ('Ivan', 'Vasiliev', '+2 (222) 222-22-22', 'ivan@gmail.com');
